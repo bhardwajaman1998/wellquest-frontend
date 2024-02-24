746f1c448f3b322673755b739577fd631a0766e1
Aman Bhardwaj <bhardwajaman1998@gmail.com>
2024-02-24 14:03:05 -0800

    Update AppStack.js

--- Stacks/AppStack.js
+++ Stacks/AppStack.js
@@ -14,7 +14,7 @@ const AppStack = () => {
   return (
   <NavigationContainer>
     <Stack.Navigator >
-        {/* <Stack.Screen
+        <Stack.Screen
           name="Onboarding"
           component={OnboardingStack}
           screenOptions = {{headerShown: false}}
@@ -23,7 +23,7 @@ const AppStack = () => {
           name="Preferences"
           component={PreferencesStack}
           options = {{headerShown: false}}
-        /> */}
+        />
         <Stack.Screen
           name="Dashboard"
           component={DashboardStack}
