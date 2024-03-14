61216f3d45aa8b7bc7c1b4ef781c851c548a64d0
Noyal Godara <ngodara01@mylangara.ca>
2024-03-14 15:41:18 -0700

    Bar graph UI/UX

--- components/progressComponents/Calorie_History.js
+++ components/progressComponents/Calorie_History.js
@@ -1,49 +1,46 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native';
-import { BarChart } from 'react-native-gifted-charts';
-
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { BarChart } from "react-native-gifted-charts";
 
 const Calorie_History = () => {
-    const barData = [
-        {value: 25, label: 'M'},
-        {value: 50, label: 'T', frontColor: '#177AD5'},
-        {value: 74, label: 'W', frontColor: '#177AD5'},
-        {value: 32, label: 'T'},
-        {value: 60, label: 'F', frontColor: '#177AD5'},
-        {value: 25, label: 'S'},
-        {value: 30, label: 'S'},
-    ];
+  const barData = [
+    { value: 25, label: "M" },
+    { value: 50, label: "T", frontColor: "#177AD5" },
+    { value: 74, label: "W", frontColor: "#177AD5" },
+    { value: 32, label: "T" },
+    { value: 60, label: "F", frontColor: "#177AD5" },
+    { value: 25, label: "S" },
+    { value: 30, label: "S" },
+  ];
   return (
     <View style={styles.container}>
-        <Text style={styles.calorieHeading}>Calorie History</Text>
-            <View style={styles.chart}>
-                <BarChart
-                barWidth={20}
-                noOfSections={1}
-                barBorderRadius={12}
-                frontColor="lightgray"
-                data={barData}
-                yAxisThickness={0}
-                xAxisThickness={0}
-                labelSize={8}
-            />
-            </View>
+      <Text style={styles.calorieHeading}>Calorie History</Text>
+      <View style={styles.chart}>
+        <BarChart
+          barWidth={20}
+          noOfSections={3}
+          barBorderRadius={12}
+          frontColor="lightgray"
+          data={barData}
+          yAxisThickness={0}
+          xAxisThickness={0}
+          labelSize={8}
+        />
+      </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container:{
-        margin: 10
-
-    },
-    calorieHeading:{
-        fontSize: 18,
-        textAlign: 'left',
-    },
-    chart:{
-        marginTop: 10
-    }
-
-})
-export default Calorie_History
+  container: {
+    margin: 10,
+  },
+  calorieHeading: {
+    fontSize: 18,
+    textAlign: "left",
+  },
+  chart: {
+    marginTop: 10,
+  },
+});
+export default Calorie_History;
