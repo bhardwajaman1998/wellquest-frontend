a906ee0afffc578514f622a75f9f9a9f083dda36
Noyal Godara <ngodara01@mylangara.ca>
2024-03-17 22:13:12 -0700

    Vertical line Component's name changed

--- dashboardFlow/ProfileScreen.js
+++ dashboardFlow/ProfileScreen.js
@@ -9,7 +9,7 @@ const ProfileScreen = () => {
         <View style={styles.profileContainer}>
           <Image style={styles.profile} source={profile} />
         </View>
-        <View style={styles.line}></View>
+        <View style={styles.Vline}></View>
         <View style={styles.userDetails}>
           <Text style={styles.userName}>Sophie Wong</Text>
           <Text>Joined</Text>
@@ -20,12 +20,17 @@ const ProfileScreen = () => {
         <TouchableOpacity>
           <Text>Edit Profile</Text>
         </TouchableOpacity>
+        <View style={styles.line}></View>
+
         <TouchableOpacity>
           <Text>Privacy Policy</Text>
         </TouchableOpacity>
+        <View style={styles.line}></View>
+
         <TouchableOpacity>
           <Text>Settings</Text>
         </TouchableOpacity>
+        <View style={styles.line}></View>
       </View>
     </View>
   )
@@ -59,7 +64,7 @@ const styles = StyleSheet.create({
     height: 100,
     width: 100,
   },
-  line: {
+  Vline: {
     width: 1,
     height: "100%",
     backgroundColor: "black",
@@ -77,83 +82,3 @@ const styles = StyleSheet.create({
 })
 
 export default ProfileScreen
-
-// import React from "react"
-// import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native"
-// import profile from "../assets/profile.jpg"
-
-// const ProfileScreen = () => {
-//   return (
-//     <View style={styles.container}>
-//       <View style={styles.firstContainer}>
-//         <View style={styles.profileContainer}>
-//           <Image style={styles.profile} source={profile} />
-//         </View>
-//         <View style={styles.line}></View>
-//         <View style={styles.userDetails}>
-//           <Text style={styles.userName}>Sophie Wong</Text>
-//           <Text>Joined</Text>
-//           <Text>2 Months Ago</Text>
-//         </View>
-//       </View>
-//       <View style={styles.secondContainer}>
-//         <TouchableOpacity>
-//           <Text>Edit Profile</Text>
-//         </TouchableOpacity>
-//         <TouchableOpacity>
-//           <Text>Privacy Policy</Text>
-//         </TouchableOpacity>
-//         <TouchableOpacity>
-//           <Text>Settings</Text>
-//         </TouchableOpacity>
-//       </View>
-//     </View>
-//   )
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignContent: "center",
-//   },
-//   firstContainer: {
-//     marginLeft: 50,
-//     marginRight: 50,
-//     justifyContent: "space-around",
-//     alignItems: "center",
-//     flexDirection: "row",
-//   },
-//   profileContainer: {
-//     height: 110,
-//     width: 110,
-//     padding: 5,
-//     borderColor: "#FF6200",
-//     borderWidth: 2,
-//     borderRadius: 75,
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-//   profile: {
-//     borderRadius: 75,
-//     height: 100,
-//     width: 100,
-//   },
-//   line: {
-//     width: 1,
-//     height: "100%",
-//     backgroundColor: "black",
-//     marginHorizontal: 10,
-//   },
-//   userDetails: {
-//     // marginLeft: 30,
-//   },
-//   userName: {
-//     fontSize: 16,
-//     fontWeight: "bold",
-//   },
-
-//   secondContainer: {},
-// })
-
-// export default ProfileScreen
