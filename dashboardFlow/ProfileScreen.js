e04fb894244f68974ccd31dc21f896f137c2d498
Noyal Godara <ngodara01@mylangara.ca>
2024-03-17 22:04:49 -0700

    Styling added to picture

--- dashboardFlow/ProfileScreen.js
+++ dashboardFlow/ProfileScreen.js
@@ -6,7 +6,14 @@ const ProfileScreen = () => {
   return (
     <View style={styles.container}>
       <View style={styles.firstContainer}>
-        <Image style={styles.profile} source={profile} />
+        <View style={styles.profileContainer}>
+          <Image style={styles.profile} source={profile} />
+        </View>
+        <View style={styles.userDetails}>
+          <Text style={styles.userName}>Sophie Wong</Text>
+          <Text>Joined</Text>
+          <Text>2 Months Ago</Text>
+        </View>
       </View>
     </View>
   )
@@ -16,16 +23,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    // alignContent: "center",
+    alignContent: "center",
   },
   firstContainer: {
     justifyContent: "center",
     alignItems: "center",
+    flexDirection: "row",
+  },
+  profileContainer: {
+    height: 110,
+    width: 110,
+    padding: 5,
+    borderColor: "#FF6200",
+    borderWidth: 2,
+    borderRadius: 75,
+    alignItems: "center",
+    justifyContent: "center",
   },
   profile: {
-    height: 150,
-    width: 150,
     borderRadius: 75,
+    height: 100,
+    width: 100,
+  },
+  userDetails: {
+    marginLeft: 30,
+  },
+  userName: {
+    fontSize: 16,
+    fontWeight: "bold",
   },
 })
 
