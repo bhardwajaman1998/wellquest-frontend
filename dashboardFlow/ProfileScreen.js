6a7ffdeef000547437d93b22dc54eeecd720a488
Noyal Godara <ngodara01@mylangara.ca>
2024-03-17 21:51:29 -0700

    Profile Image Styling Added

--- dashboardFlow/ProfileScreen.js
+++ dashboardFlow/ProfileScreen.js
@@ -1,8 +1,32 @@
 import React from "react"
-import { Text, View } from "react-native"
+import { Image, StyleSheet, Text, View } from "react-native"
+import profile from "../assets/profile.jpg"
 
 const ProfileScreen = () => {
-  return <Text>NOYAL PRofile</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.firstContainer}>
+        <Image style={styles.profile} source={profile} />
+      </View>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    // alignContent: "center",
+  },
+  firstContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  profile: {
+    height: 150,
+    width: 150,
+    borderRadius: 75,
+  },
+})
 
 export default ProfileScreen
